9a05e751ce15ade7c95826ede1defd454d06f88f
FixTheInternet <vmpatoleia@protonmail.com>
2018-10-21 18:51:29 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 // External dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb')
+// C:\Program Files\MongoDB\Server\4.0\bin
+// mongod --port 27017 --dbpath "C:\Users\vmpat\mongo-data"
 // Local dependencies 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
 
 // Express setup
 var app = express();
@@ -32,6 +35,25 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     })
+})
+
+// GET Individual TODO route
+app.get('/todos/:id', (req, res) => {
+  if(!ObjectID.isValid(req.params.id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(req.params.id).then((todo) => {
+    
+    if(!todo) {
+      res.status(404).send()
+    }
+    
+    res.send({todo})
+
+  }).catch((err) => {
+    res.status(404).send()
+  })
 })
 
 
