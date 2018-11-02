ab7e5e5f8b9e2f9be2c20f4e6744b1787faad317
FixTheInternet <vmpatoleia@protonmail.com>
2018-11-02 17:27:59 +0100

    Test DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -117,13 +117,50 @@ describe('GET /todos/:id', () => {
   })
 })
 
-obj = {
-  name: 'carl',
-  sayName: function() {
-    
-    return func
-  }
-}
+describe('DELETE /todos:id', () => {
+
+  var hexId = todos[1]._id.toHexString();
+
+  it('should remove a todo', (done) => {
+
+    request(app)
+    .delete(`/todos/${hexId}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo._id).toBe(hexId)
+    })
+    .end((err, res) => {
+      if (err) {
+        return done(err)
+      }
+
+      Todo.findById(hexId).then((todo) => {
+        expect(todo).toNotExist()
+        done()
+      }).catch(err => {
+        done(err)
+      })
+
+    })
+
+  })
+
+  it('should return 404 if todo not found', (done) => {
+    let hexId = new ObjectID().toHexString()
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done)
+  })
+
+  it('should return 404 if objectId is invalid', (done) => {
+    request(app)
+    .delete(`/todos/1234`)
+    .expect(404)
+    .end(done)
+  })
+
+})
 
 
 
